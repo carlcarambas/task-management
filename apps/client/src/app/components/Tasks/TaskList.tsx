be25226ea2d4e24dca9464c6c07ccec4c92ee475
Carl Mark Carambas <36352083+carlcarambas@users.noreply.github.com>
2025-04-18 13:05:59 +0800

    ui improvement

--- apps/client/src/app/components/Tasks/TaskList.tsx
+++ apps/client/src/app/components/Tasks/TaskList.tsx
@@ -1,34 +1,12 @@
 import { TTask } from '../../app.types';
 
 interface TaskListProps {
-  isLoading: boolean;
   tasks: TTask[];
   onDelete?: (id: string) => void;
   onToggleComplete?: (task: TTask) => void;
 }
 
-const TaskList = ({
-  isLoading,
-  tasks,
-  onDelete,
-  onToggleComplete,
-}: TaskListProps) => {
-  if (isLoading) {
-    return (
-      <div className="py-4" data-testid="task-list-loading">
-        <div className="animate-pulse space-y-4">
-          {[1, 2, 3].map((item) => (
-            <div
-              key={item}
-              className="h-16 bg-gray-200 rounded"
-              data-testid="task-loader"
-            ></div>
-          ))}
-        </div>
-      </div>
-    );
-  }
-
+const TaskList = ({ tasks, onDelete, onToggleComplete }: TaskListProps) => {
   return (
     <div className="mt-4" data-testid="task-list">
       <ul className="divide-y divide-gray-200">
@@ -36,12 +14,34 @@ const TaskList = ({
           <li key={task._id} className="py-4">
             <div className="flex items-center justify-between">
               <div className="flex items-center">
-                <input
+                {/* <input
                   type="checkbox"
                   checked={task.completed}
                   onChange={() => onToggleComplete?.(task)}
-                  className="h-5 w-5 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                />
+                  className="h-5 w-5 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded cursor-pointer"
+                  title="Mark as completed"
+                /> */}
+                <div className="relative flex items-center group">
+                  <input
+                    type="checkbox"
+                    checked={task.completed}
+                    onChange={() => onToggleComplete?.(task)}
+                    className="h-5 w-5 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded cursor-pointer peer"
+                    aria-label={
+                      task.completed ? 'Mark as incomplete' : 'Mark as complete'
+                    }
+                  />
+                  {/* Tooltip */}
+                  <span
+                    className="absolute bottom-6 ml-2 px-2 py-1 text-xs text-white bg-gray-800 rounded opacity-0 
+                  pointer-events-none transition-opacity duration-200
+                  group-hover:opacity-100 peer-focus:opacity-100"
+                  >
+                    {task.completed
+                      ? 'Mark as incomplete'
+                      : 'Mark as completed'}
+                  </span>
+                </div>
                 <span
                   className={`ml-3 ${
                     task.completed
